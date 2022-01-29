8d76c8a6d49898a530e68bb55f06fc0af262b028
caleb1-valo <65986150+caleb1-valo@users.noreply.github.com>
2022-01-29 22:27:00 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2c20Db15aE64c332f08D61bf16fe90D39F3BdA45";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-frontend-beta.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+
+    const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+    const metadataURL = METADATA_URL;
+
+    const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+    const deployedCryptoDevsContract = await cryptoDevsContract.deploy(metadataURL, whitelistContract);
+
+    // print the address of the deployed contract
+    console.log(
+        "Crypto Devs Contract Address:",
+        deployedCryptoDevsContract.address
+    );
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
